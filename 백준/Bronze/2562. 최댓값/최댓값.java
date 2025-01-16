eb253ab837ae797e428b81876abd8ecb99635129
hwangsehee <65377385+sehee123@users.noreply.github.com>
2025-01-16 18:03:25 +0900

    [Bronze III] Title: 최댓값, Time: 100 ms, Memory: 14196 KB -BaekjoonHub

---
+++ 백준/Bronze/2562. 최댓값/최댓값.java
@@ -0,0 +1,21 @@
+import java.io.*;
+
+public class Main{
+
+    public static void main(String [] args)throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int max = 0 ;
+        int idx = 0;
+        for(int i = 1; i<=9; i++){   
+            int num = Integer.parseInt(br.readLine());
+            if(max <  num){
+                max = num;
+                idx = i;
+            }
+        }
+        System.out.println(max);
+        System.out.println(idx);
+        
+    }
+
+}
