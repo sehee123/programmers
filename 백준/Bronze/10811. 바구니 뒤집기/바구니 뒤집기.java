b2b4a0512b8f646bcde01b8c9c0b25ea63a6be26
hwangsehee <65377385+sehee123@users.noreply.github.com>
2025-01-18 13:57:10 +0900

    [Bronze II] Title: 바구니 뒤집기, Time: 120 ms, Memory: 15828 KB -BaekjoonHub

--- 백준/Bronze/10811. 바구니 뒤집기/바구니 뒤집기.java
+++ 백준/Bronze/10811. 바구니 뒤집기/바구니 뒤집기.java
@@ -2,35 +2,38 @@ import java.io.*;
 import java.util.*;
 
 public class Main{
-    public static void main(String []args )throws IOException{
+   public static void main(String []args )throws IOException{
+
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String [] numArr = br.readLine().split(" ");
         int basketCnt = Integer.parseInt(numArr[0]);
         int loofCnt = Integer.parseInt(numArr[1]);
-        
+
         int [] resultArr = new int [basketCnt+1];
-        
+
         for(int i =1; i<= basketCnt; i++){
             resultArr[i] = i;
         }
-        
+
         for(int i = 0 ; i<loofCnt; i++){
             String [] basketArr = br.readLine().split(" ");
             int start = Integer.parseInt(basketArr[0]);
             int end = Integer.parseInt(basketArr[1]);
-            int [] tmp = new int [basketCnt +1];
-            for(int j=start ; j<=end; j++){
-                tmp[j] = resultArr[j];
+
+            while(start<end){
+                int tmp = resultArr[start];
+                resultArr[start] = resultArr[end];
+                resultArr[end] = tmp;
+                start ++;
+                end --;
             }
-            for(int j=start ; j<=end; j++){
-                 resultArr[end-j+start] = tmp[j];
-            }
-            
+
         }
-        
-        for(int i =1; i<=basketCnt; i++){
-            System.out.print(resultArr[i]+" ");
-            
+
+        StringBuilder sb = new StringBuilder();
+        for(int i = 1; i<=basketCnt;i++){
+            sb.append(resultArr[i]+" ");
         }
+        System.out.println(sb.toString().trim());
     }
 }
