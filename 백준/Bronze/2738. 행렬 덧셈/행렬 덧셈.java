fc50ebaee6a24be3d6085e82893b4403d56c1951
hwangsehee <65377385+sehee123@users.noreply.github.com>
2025-01-23 16:05:57 +0900

    [Bronze III] Title: 행렬 덧셈, Time: 156 ms, Memory: 16836 KB -BaekjoonHub

--- 백준/Bronze/2738. 행렬 덧셈/행렬 덧셈.java
+++ 백준/Bronze/2738. 행렬 덧셈/행렬 덧셈.java
@@ -5,25 +5,24 @@ public class Main {
          BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         String[] NM = br.readLine().split(" ");
+        
         int n = Integer.parseInt(NM[0]);
         int m = Integer.parseInt(NM[1]);
-        int[][] sumArr = new int[2*n][m];
-        for (int i = 0; i < n; i++) {
-            String[] arr = br.readLine().split(" ");
-            for (int j = 0; j < m; j++) {
-                sumArr[i][j] = Integer.parseInt(arr[j]);
-            }
-        }
-        for (int i = n; i < 2 * n; i++) {
-            String[] arr = br.readLine().split(" ");
-            for (int j = 0; j < m; j++) {
-                sumArr[i][j] = Integer.parseInt(arr[j]);
+        
+        int[][] sumArr = new int[n][m];
+        
+        for(int i = 0; i<2; i++){
+        	for(int j = 0; j<n; j++){
+            String [] arr =  br.readLine().split(" ");
+            	for(int k=0;k<m; k++){
+                	sumArr[j][k]+=Integer.parseInt(arr[k]);
+                }
             }
         }
         StringBuilder sb = new StringBuilder();
         for(int i = 0; i< n; i++){
            for(int j = 0 ; j<m ; j++){
-              sb.append(sumArr[i][j]+sumArr[i+n][j]).append(" ");
+              sb.append(sumArr[i][j]).append(" ");
            }
            sb.append("\n");
         }
