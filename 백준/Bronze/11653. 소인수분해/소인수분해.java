b9e5730bee292907d388f9ddff5f1f908a63292c
hwangsehee <65377385+sehee123@users.noreply.github.com>
2025-01-27 16:13:43 +0900

    [Bronze I] Title: 소인수분해, Time: 104 ms, Memory: 14268 KB -BaekjoonHub

--- 백준/Bronze/11653. 소인수분해/소인수분해.java
+++ 백준/Bronze/11653. 소인수분해/소인수분해.java
@@ -3,33 +3,20 @@ import java.util.*;
 
 public class Main {
     
-    public static List<Integer> list = new ArrayList<>();
-    
     public static void main(String [] args )throws IOException{
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        
+       BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+       StringBuilder sb = new StringBuilder();
+       
        int a = Integer.parseInt(br.readLine());
-        List <Integer> list = new ArrayList<>();
-        int i = 2;
-        while(a >1){
-            if(a%i == 0 && isPrime(i)){
-                while(a%i == 0){
-                    list.add(i);
-                    a/=i;
-                }
+        
+       for(int i = 2; i*i<=a;i++){
+       		while(a%i == 0){
+            	sb.append(i).append("\n");
+                a/=i;
             }
-            i++;
-        }
-        if(list.size() > 0){
-            for (Integer integer : list) {
-                System.out.println(integer);
-            }
-        }
-    }
-     private static boolean isPrime(int num){
-        for(int i = 2 ; i<num; i++){
-            if(num %i ==0)return false;
-        }
-        return true;
+       }
+       if(a!= 1)sb.append(a);
+       
+       System.out.print(sb);
     }
 }
