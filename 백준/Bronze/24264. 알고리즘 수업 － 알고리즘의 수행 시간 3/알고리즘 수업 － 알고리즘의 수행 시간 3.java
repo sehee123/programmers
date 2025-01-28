d2007de7267d6bb04e134353389fd2bba26b6b6e
hwangsehee <65377385+sehee123@users.noreply.github.com>
2025-01-28 19:37:04 +0900

    [Bronze III] Title: 알고리즘 수업 - 알고리즘의 수행 시간 3, Time: 108 ms, Memory: 14140 KB -BaekjoonHub

--- 백준/Bronze/24264. 알고리즘 수업 － 알고리즘의 수행 시간 3/알고리즘 수업 － 알고리즘의 수행 시간 3.java
+++ 백준/Bronze/24264. 알고리즘 수업 － 알고리즘의 수행 시간 3/알고리즘 수업 － 알고리즘의 수행 시간 3.java
@@ -2,10 +2,11 @@ import java.io.*;
 
 public class Main{
     
-    public static void main(String [] args)throws IOException{
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        long n = Integer.parseInt(br.readLine());
-        System.out.print(n*n+"\n2");
+        long N = Long.parseLong(br.readLine());
+        System.out.println(N * N);
+        System.out.println(2);
     }
     
 }
