b5230e50505e61d0653b66504c81d886278f4adf
hwangsehee <65377385+sehee123@users.noreply.github.com>
2025-01-29 01:08:08 +0900

    [Bronze II] Title: 알고리즘 수업 - 알고리즘의 수행 시간 6, Time: 104 ms, Memory: 14160 KB -BaekjoonHub

--- 백준/Bronze/24267. 알고리즘 수업 － 알고리즘의 수행 시간 6/알고리즘 수업 － 알고리즘의 수행 시간 6.java
+++ 백준/Bronze/24267. 알고리즘 수업 － 알고리즘의 수행 시간 6/알고리즘 수업 － 알고리즘의 수행 시간 6.java
@@ -5,11 +5,7 @@ public class Main{
     public static void main(String [] args)throws IOException{
          BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
-        long sum = 0;
-        for(int i = 1; i<n ; i++){
-           sum += ((long)i*(i-1))/2;
-        }
-        System.out.println(sum);
+         System.out.println(((long)n*(n-1)*(n-2))/6);
         System.out.print(3);
     }
     
