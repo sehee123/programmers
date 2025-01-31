7ae8b020536822f071f5bbe27e931b3d4c325fa3
hwangsehee <65377385+sehee123@users.noreply.github.com>
2025-01-31 21:45:50 +0900

    [Silver V] Title: 영화감독 숌, Time: 264 ms, Memory: 86256 KB -BaekjoonHub

---
+++ 백준/Silver/1436. 영화감독 숌/영화감독 숌.java
@@ -0,0 +1,22 @@
+import java.io.*;
+
+public class Main {
+    
+    public static void main(String []  args)throws IOException{
+        
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int cnt = 0;
+        int num = 666;
+        while(cnt < n){
+             if(String.valueOf(num).contains("666")){
+                cnt ++;
+                if(cnt == n)break;
+            }
+            num++;
+        }
+        System.out.print(num);
+        
+    }
+    
+}
