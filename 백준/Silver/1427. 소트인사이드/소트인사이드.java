79e5452bc28b2c7ea49ed3d7c0c9ede726115d64
hwangsehee <65377385+sehee123@users.noreply.github.com>
2025-02-01 00:43:31 +0900

    [Silver V] Title: 소트인사이드, Time: 100 ms, Memory: 14324 KB -BaekjoonHub

--- 백준/Silver/1427. 소트인사이드/소트인사이드.java
+++ 백준/Silver/1427. 소트인사이드/소트인사이드.java
@@ -3,21 +3,17 @@ import java.util.*;
 
 public class Main{
     
-    public static void main(String [] args)throws IOException{
+     public static void main(String [] args)throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
+
 
         String strN = br.readLine();
         int len = strN.length();
-        int [] arr = new int[len];
-        for(int i = 0; i<len; i++){
-            arr[i] = strN.charAt(i)-'0';
-        }
+
+        char [] arr = strN.toCharArray();
         Arrays.sort(arr);
-        for(int i= len-1; i>=0; i--){
-            sb.append(arr[i]);
-        }
-        System.out.println(sb);
+
+        System.out.println(new StringBuilder(new String(arr)).reverse().toString());
     }
     
 }
