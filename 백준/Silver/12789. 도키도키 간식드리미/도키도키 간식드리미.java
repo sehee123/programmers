39d26745e98b8ae9e034f86ac48009d1e607ccfc
hwangsehee <65377385+sehee123@users.noreply.github.com>
2025-02-10 15:49:21 +0900

    [Silver III] Title: 도키도키 간식드리미, Time: 112 ms, Memory: 14428 KB -BaekjoonHub

--- 백준/Silver/12789. 도키도키 간식드리미/도키도키 간식드리미.java
+++ 백준/Silver/12789. 도키도키 간식드리미/도키도키 간식드리미.java
@@ -2,49 +2,35 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    
-    static Stack <Integer> stop = new Stack<>();
-    static Stack <Integer> des = new Stack<>();
-    
     public static void main(String [] args)throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         int n = Integer.parseInt(br.readLine());
         StringTokenizer st = new StringTokenizer(br.readLine());
-
+        int idx = 1;
+        Stack<Integer> stack = new Stack<>();
         for(int i = 0; i<n ; i++){
             int num = Integer.parseInt(st.nextToken());
-            chkStack(num);
+            if(idx == num ){
+                idx ++;
+            }else {
+                while(!stack.isEmpty() && stack.peek() == idx){
+                    stack.pop();
+                    idx ++;
+                }
+                stack.push(num);
+            }
         }
 
-        if(!des.isEmpty())chkStop();
+        while(!stack.isEmpty() && stack.peek() == idx){
+            stack.pop();
+            idx ++;
+        }
 
-        if(des.size() == n){
+        if(idx-1 == n){
             System.out.println("Nice");
         }else{
             System.out.println("Sad");
-        }
-    }
-    private static void chkStack(int num){
-        if(des.isEmpty()){
-            if(num ==1){
-                des.push(num);
-            }else{
-                stop.push(num);
-            }
-        }else{
-           chkStop();
-           if(des.peek()+1 == num){
-                des.push(num);
-            }else {
-                stop.push(num);
-            }
-        }
-    }
-    private static void chkStop(){
-        while(!stop.isEmpty()&&stop.peek() == des.peek()+1){
-            des.push(stop.peek());
-            stop.pop();
         }
     }
     
