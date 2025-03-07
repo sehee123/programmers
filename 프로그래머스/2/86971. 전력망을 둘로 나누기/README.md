074d080fa4033cd6416ec6ef237e30dd47bf8c19
hwangsehee <65377385+sehee123@users.noreply.github.com>
2025-03-07 22:13:31 +0900

    [level 2] Title: 전력망을 둘로 나누기, Time: 6.52 ms, Memory: 87.3 MB -BaekjoonHub

--- 프로그래머스/2/86971. 전력망을 둘로 나누기/README.md
+++ 프로그래머스/2/86971. 전력망을 둘로 나누기/README.md
@@ -1,10 +1,10 @@
 # [level 2] 전력망을 둘로 나누기 - 86971 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/86971?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/86971) 
 
 ### 성능 요약
 
-메모리: 90.6 MB, 시간: 5.93 ms
+메모리: 87.3 MB, 시간: 6.52 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 12월 23일 22:14:35
+2025년 03월 07일 22:13:28
 
 ### 문제 설명
 
--- 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
+++ 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
@@ -1,58 +1,50 @@
-import java.util.ArrayList;
-import java.util.List;
-
+import java.util.*;
 class Solution {
-    
-    static List<boolean []> visitedList = new ArrayList<>();
-    
     public int solution(int n, int[][] wires) {
         
         List<List<Integer>> graph = new ArrayList<>();
         
-        for(int i = 0 ; i<=n; i++){
+        for(int i = 0; i<=n; i++){
             graph.add(new ArrayList<>());
         }
-        
-        for(int [] wire : wires){
+        for(int []wire : wires){
             graph.get(wire[0]).add(wire[1]);
             graph.get(wire[1]).add(wire[0]);
         }
         
-        for(int i = 0; i<wires.length; i++){
-            visitedList.add(new boolean[n+1]);
-            graph.get(wires[i][0]).remove(Integer.valueOf(wires[i][1]));
-            graph.get(wires[i][1]).remove(Integer.valueOf(wires[i][0]));
-            dfs(1,graph, i);
-            graph.get(wires[i][0]).add(Integer.valueOf(wires[i][1]));
-            graph.get(wires[i][1]).add(Integer.valueOf(wires[i][0]));
+        int minDiff = Integer.MAX_VALUE; 
+        
+        for(int [] wire : wires){
+            int nodeA = wire[0];
+            int nodeB = wire[1];
+            
+            graph.get(nodeA).remove(Integer.valueOf(nodeB)); 
+            graph.get(nodeB).remove(Integer.valueOf(nodeA));
+            //remove(int idx);
+            //remove(Object obj); 차이점 
+            boolean [] visited = new boolean [n+1];
+            int cnt = dfs(nodeA , graph,visited);
+            
+            int diff = Math.abs(n - cnt - cnt );
+            minDiff = Math.min(minDiff , diff);
+            
+            graph.get(nodeA).add(nodeB); 
+            graph.get(nodeB).add(nodeA);
         }
         
-        int num = Integer.MAX_VALUE;
         
-        for(boolean [] arr : visitedList){
-            int nodeCnt = 0 ;
-            for(int i = 0 ; i<arr.length; i++){
-                if(arr[i]){
-                    nodeCnt ++ ;
-                }
-            }
-            int abs = Math.abs(n - (2*nodeCnt));
-            if(abs < num){
-                num = abs;
-            }
-        }
-        
-        return num;
+        return minDiff;
     }
-    
-    public static void dfs(int node,List<List<Integer>> graph, int idx ){
-        visitedList.get(idx)[node] = true;
-
-        for(int neighbor : graph.get(node)){
-            if(!visitedList.get(idx)[neighbor]){
-                dfs(neighbor,graph,idx);
+    public static int dfs(int node , List<List<Integer>> graph , boolean [] visited){
+        visited[node] = true;
+        int cnt = 1; 
+        
+        for(int neighbor :graph.get(node)){
+            if(!visited[neighbor]){
+                cnt+=dfs(neighbor,graph,visited);
             }
         }
+        
+        return cnt ;
     }
-    
 }
