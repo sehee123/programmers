dd255e35abc5dbaeff7a8b4bf19598b4f5bb8e56
hwangsehee <65377385+sehee123@users.noreply.github.com>
2025-08-07 20:19:30 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 43.14 ms, Memory: 111 MB -BaekjoonHub

--- 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.java
+++ 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.java
@@ -2,21 +2,16 @@ import java.util.*;
 
 public class Solution {
     public int[] solution(int []arr) {
- 
-        Deque<Integer> deque = new LinkedList<>();
+       
+        List<Integer> duplRemoveList = new ArrayList<>();
+        duplRemoveList.add(arr[0]);
         
-        for(int i = 0; i<arr.length ; i++){
-            if(deque.isEmpty()){
-                deque.offerLast(arr[i]);
-            }else if(deque.peekLast() != arr[i]){
-                deque.offerLast(arr[i]);
+        for(int i =0; i<arr.length;i++){
+            if(arr[i] != duplRemoveList.get(duplRemoveList.size()-1)){
+                duplRemoveList.add(arr[i]);
             }
         }
-        int[] answer = new int [deque.size()];
         
-        for(int i =0; i<answer.length;i++){
-            answer [i] = deque.pollFirst(); 
-        }
-        return answer;
+        return duplRemoveList.stream().mapToInt(i->i).toArray();
     }
 }
