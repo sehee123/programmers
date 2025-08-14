46833235dc3e0fda39c6d6ebe026ea4b236e330a
hwangsehee <65377385+sehee123@users.noreply.github.com>
2025-08-14 23:42:04 +0900

    [Bronze V] Title: 킹, 퀸, 룩, 비숍, 나이트, 폰, Time: 100 ms, Memory: 14204 KB -BaekjoonHub

--- 백준/Bronze/3003. 킹， 퀸， 룩， 비숍， 나이트， 폰/킹， 퀸， 룩， 비숍， 나이트， 폰.java
+++ 백준/Bronze/3003. 킹， 퀸， 룩， 비숍， 나이트， 폰/킹， 퀸， 룩， 비숍， 나이트， 폰.java
@@ -1,15 +1,25 @@
-import java.io.*;
 
-public class Main{
-    public static void main(String[] args )throws IOException{
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String [] inputArr = br.readLine().split(" ");
-        int [] chess = {1,1,2,2,2,8};
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    public static void main(String[] args)throws IOException {
+        int [] correctCnt = {1,1,2,2,2,8};
+
         StringBuilder sb = new StringBuilder();
-        for(int i = 0; i< 6; i++){
-            sb.append(chess[i] - Integer.parseInt(inputArr[i]));
-            sb.append(" ");
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String [] brArr = br.readLine().split(" ");
+
+        for(int i =0; i<6; i++) {
+            sb.append(correctCnt[i] - Integer.parseInt(brArr[i]));
+            if (i != 5) {
+                sb.append(" ");
+            }
         }
-        System.out.println(sb);
+        System.out.println(sb.toString());
     }
 }
