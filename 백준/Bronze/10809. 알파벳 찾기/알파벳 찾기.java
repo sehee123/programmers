3998570ecb95dbbea618efb239a94d4110da85b9
hwangsehee <65377385+sehee123@users.noreply.github.com>
2025-08-18 17:44:36 +0900

    [Bronze II] Title: 알파벳 찾기, Time: 120 ms, Memory: 15916 KB -BaekjoonHub

--- 백준/Bronze/10809. 알파벳 찾기/알파벳 찾기.java
+++ 백준/Bronze/10809. 알파벳 찾기/알파벳 찾기.java
@@ -1,27 +1,27 @@
 import java.io.*;
 import java.util.*;
 
-public class Main{
-    
-    public static void main(String [] args)throws IOException{
+public class Main {
+   public static void main(String[] args)throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         String str = br.readLine();
-        int [] alphabetArr = new int [26];
-        Arrays.fill(alphabetArr,-1);
-        for(int i = 0; i<str.length();i++){
-            int Ascii = (int)str.charAt(i);
-            int idx = Ascii - 97;
-            if(alphabetArr[idx] == -1)alphabetArr[idx] = i;        
+
+        int [] answer = new int [26];
+        Arrays.fill(answer, -1);
+
+         for(int i =0; i < str.length(); i++) {
+            char c = str.charAt(i);
+            if(answer [c - 'a'] == -1) {
+                answer[c - 'a'] = i;
+            }
         }
-        StringBuilder sb = new StringBuilder();
-        for(int i = 0; i<alphabetArr.length; i++){
-            sb.append(alphabetArr[i]+" ");
+
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        for(int num : answer) {
+            bw.write(num + " ");
         }
-        bw.write(sb.toString());
         bw.flush();
         bw.close();
         br.close();
     }
-    
 }
