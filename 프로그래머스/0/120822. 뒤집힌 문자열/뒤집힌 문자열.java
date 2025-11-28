5eac54057faecb35b310d717311a2a47a181243c
hwangsehee <65377385+sehee123@users.noreply.github.com>
2025-11-28 15:49:47 +0900

    [level 0] Title: 뒤집힌 문자열, Time: 2.07 ms, Memory: 69.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/120822. 뒤집힌 문자열/뒤집힌 문자열.java
@@ -0,0 +1,13 @@
+class Solution {
+    public String solution(String my_string) {
+        String answer = "";
+        
+        String [] arr = my_string.split("");
+        
+        for(int i = arr.length-1; i>=0; i--){
+            answer +=arr[i];
+        }
+        
+        return answer;
+    }
+}
