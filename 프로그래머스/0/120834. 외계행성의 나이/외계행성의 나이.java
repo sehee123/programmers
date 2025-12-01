4fff0f8b3e324f5f785cc7a45599a9b2e26c752f
hwangsehee <65377385+sehee123@users.noreply.github.com>
2025-12-01 13:16:31 +0900

    [level 0] Title: 외계행성의 나이, Time: 0.06 ms, Memory: 70.9 MB -BaekjoonHub

--- 프로그래머스/0/120834. 외계행성의 나이/외계행성의 나이.java
+++ 프로그래머스/0/120834. 외계행성의 나이/외계행성의 나이.java
@@ -1,16 +1,16 @@
 class Solution {
     public String solution(int age) {
-        String answer = "";
+       
         
-        String str = String.valueOf(age); 
-        String [] arr = str.split("");
+        String str = String.valueOf(age);
+        StringBuilder sb = new StringBuilder();
         
-        for(int i =0; i<arr.length; i++){
-            char c = (char)(97+Integer.parseInt(arr[i]));
-            answer +=c;
+        for(int i =0; i<str.length(); i++){
+            int num = str.charAt(i) - '0';
+            char c = (char)('a' + num);
+            sb.append(c);
         }
         
-        
-        return answer;
+        return sb.toString();
     }
 }
