912cb9c75a4a5859971d6111ae41d4ff989d5b39
hwangsehee <65377385+sehee123@users.noreply.github.com>
2025-12-02 11:39:15 +0900

    [level 0] Title: 개미 군단, Time: 0.02 ms, Memory: 76.3 MB -BaekjoonHub

--- 프로그래머스/0/120837. 개미 군단/개미 군단.java
+++ 프로그래머스/0/120837. 개미 군단/개미 군단.java
@@ -3,13 +3,12 @@ class Solution {
         int answer = 0;
         
         answer += hp/5;
+        hp %= 5; 
         
-        if(hp%5 != 0){
-            answer += hp%5/3; 
-            if((hp%5)%3 !=0){
-                answer += (hp%5)%3;
-            }
-        }
+        answer += hp/3;
+        hp %= 3;
+        
+        answer += hp/1;
         
         return answer;
     }
