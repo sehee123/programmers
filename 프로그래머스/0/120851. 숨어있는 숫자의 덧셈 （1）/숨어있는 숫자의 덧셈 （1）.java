0088585b7bcdecee4693d6c52b27e7a8e54ae4d9
hwangsehee <65377385+sehee123@users.noreply.github.com>
2025-12-03 11:34:42 +0900

    [level 0] Title: 숨어있는 숫자의 덧셈 (1), Time: 2.51 ms, Memory: 82 MB -BaekjoonHub

---
+++ 프로그래머스/0/120851. 숨어있는 숫자의 덧셈 （1）/숨어있는 숫자의 덧셈 （1）.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+class Solution {
+    public int solution(String my_string) {
+       return my_string.chars()
+           .filter(Character::isDigit)
+           .map(c -> c - '0')
+           .sum();
+           
+    }
+}
