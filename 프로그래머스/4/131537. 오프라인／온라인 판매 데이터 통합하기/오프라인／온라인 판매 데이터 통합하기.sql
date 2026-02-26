ca668e9b2ddba2b2a71ca88a394959d60107e911
hwangsehee <65377385+sehee123@users.noreply.github.com>
2026-02-26 19:29:36 +0900

    [level 4] Title: 오프라인/온라인 판매 데이터 통합하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/4/131537. 오프라인／온라인 판매 데이터 통합하기/오프라인／온라인 판매 데이터 통합하기.sql
+++ 프로그래머스/4/131537. 오프라인／온라인 판매 데이터 통합하기/오프라인／온라인 판매 데이터 통합하기.sql
@@ -1,11 +1,13 @@
--- 코드를 입력하세요
-select * from (
-SELECT to_char(sales_date,'yyyy-mm-dd')as sales_date, product_id , user_id , sales_amount 
-from online_sale 
-where to_char(sales_date,'yyyymm') = 202203 
+select to_char(sales_date,'yyyy-mm-dd'), product_id, user_id, sales_amount from (
+SELECT sales_date, product_id, user_id, sales_amount
+from online_sale
+where sales_date >= to_date('2022-03-01', 'yyyy-mm-dd') 
+and sales_date < to_date('2022-04-01', 'yyyy-mm-dd')
 union all 
-SELECT to_char(sales_date,'yyyy-mm-dd')as sales_date, product_id , NULL , sales_amount 
-from offline_sale 
-where to_char(sales_date,'yyyymm') = 202203 
-)
-order by sales_date  , product_id , user_id 
+SELECT sales_date, product_id, NULL, sales_amount
+from offline_sale
+where sales_date >= to_date('2022-03-01', 'yyyy-mm-dd') 
+and sales_date < to_date('2022-04-01', 'yyyy-mm-dd')
+    )
+    order by sales_date, product_id , user_id
+;
