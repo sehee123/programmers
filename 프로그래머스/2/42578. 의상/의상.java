cc44f42d005ea17778272fe0a1fd1c93e286f07d
hwangsehee <65377385+sehee123@users.noreply.github.com>
2026-02-27 17:04:04 +0900

    [level 2] Title: 의상, Time: 0.16 ms, Memory: 82.8 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.java
+++ 프로그래머스/2/42578. 의상/의상.java
@@ -3,17 +3,17 @@ import java.util.*;
 class Solution {
     public int solution(String[][] clothes) {
         
-        Map<String, Integer> categories = new HashMap<>();
-        
-        for(String cloth[]: clothes){
-            categories.put(cloth[1], categories.getOrDefault(cloth[1],0)+1);
-        }
-        
         int answer = 1;
-        for(String category: categories.keySet()){
-            int cnt = categories.get(category);
-            answer = answer *(cnt+1);
+        
+        Map<String,Integer> map = new HashMap<>();
+        for(String [] arr : clothes){
+            map.put(arr[1], map.getOrDefault(arr[1],1)+1);
         }
+        
+        for(Map.Entry<String,Integer> entry : map.entrySet()){
+            answer *= entry.getValue();
+        }
+        
         return --answer;
     }
 }
