08ab7afb7093233ddf96d000790a800df9c2d7f3
hwangsehee <65377385+sehee123@users.noreply.github.com>
2026-02-28 02:10:42 +0900

    [level 2] Title: 더 맵게, Time: 1521.87 ms, Memory: 146 MB -BaekjoonHub

--- 프로그래머스/2/42626. 더 맵게/더 맵게.java
+++ 프로그래머스/2/42626. 더 맵게/더 맵게.java
@@ -1,24 +1,20 @@
-import java.util.*;
+import java.util.* ;
+
 class Solution {
     public int solution(int[] scoville, int K) {
         int answer = 0;
-        PriorityQueue <Integer> pq = new PriorityQueue<>(); 
         
-        for(int s : scoville){
-            pq.offer(s);
-        }
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        for(int s : scoville)pq.add(s);
         
-        while(pq.size()>1 && pq.peek() < K){
+        while(pq.size() >1 && pq.peek()<K){
             int a = pq.poll();
             int b = pq.poll();
-            int sum = a + b*2;
-            pq.offer(sum);
-            
-            answer ++;
+            int mixed = a + (2*b);
+            pq.add(mixed);
+            answer++;
         }
         
-        if(pq.size () == 1 && pq.peek()<K) answer = -1;
-        
-        return answer;
+       return (pq.peek() >= K ) ? answer : -1;
     }
 }
