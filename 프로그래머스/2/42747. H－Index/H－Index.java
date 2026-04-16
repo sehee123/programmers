3c727872ff2347f504f6bded22e052900f9cb7bf
hwangsehee <65377385+sehee123@users.noreply.github.com>
2026-04-17 00:19:01 +0900

    [level 2] Title: H-Index, Time: 0.94 ms, Memory: 78.3 MB -BaekjoonHub

--- 프로그래머스/2/42747. H－Index/H－Index.java
+++ 프로그래머스/2/42747. H－Index/H－Index.java
@@ -1,16 +1,17 @@
 import java.util.*;
+
 class Solution {
     public int solution(int[] citations) {
         int answer = 0;
+        
         Arrays.sort(citations);
         int len = citations.length;
-        // 5 6 7 8 
-        for(int i =len; i>1; i--){
-            if(citations[i-1] >=i && citations[len-i]>=i){
-                answer = i;
-                break;
-            }
+        
+        for(int i = 0; i<len; i++){
+            int gap = len-i; 
+            if(citations[i]>=gap)return gap;
         }
+        
         return answer;
     }
 }
