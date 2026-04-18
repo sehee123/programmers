f5932d95942b03edf783feece577cc66ac063233
hwangsehee <65377385+sehee123@users.noreply.github.com>
2026-04-18 16:30:08 +0900

    [level 1] Title: 모의고사, Time: 9.20 ms, Memory: 80.7 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.java
+++ 프로그래머스/1/42840. 모의고사/모의고사.java
@@ -1,25 +1,47 @@
 import java.util.*;
 class Solution {
     public int[] solution(int[] answers) {
-        int[] answer = {0,0,0};
+      
         
-        int [] person1 = {1,2,3,4,5}; //5
-        int [] person2 = {2,1,2,3,2,4,2,5}; // 8
-        int [] person3 = {3,3,1,1,2,2,4,4,5,5}; //10
+        int [] first = {1, 2, 3, 4, 5}; //5
+        int [] second = {2, 1, 2, 3, 2, 4, 2, 5};//8
+        int [] third = {3, 3, 1, 1, 2, 2, 4, 4, 5, 5}; //10
         
-        for(int i = 0; i<answers.length ; i++){
-            if(answers[i] == person1[i%5])answer[0]++;
-            if(answers[i] == person2[i%8])answer[1]++;
-            if(answers[i] == person3[i%10])answer[2]++;
-        }
+        int [] correct = {0,0,0};
         
-        int max = Math.max(answer[0],Math.max(answer[1],answer[2]));
-        List<Integer> list = new ArrayList<>();
-        for(int i = 0; i< 3; i++){
-            if(answer[i]== max){
-                list.add(i+1);
+        for(int i =0; i<answers.length ; i++){
+            
+            int f = i;
+            if(i>= 5)f%=5;
+            if(answers[i] == first[f]){
+                
+                correct[0]++;
             }
+            
+            int s= i;
+            if(s>= 8)s%=8;
+            if(answers[i] == second[s]){
+                correct[1]++;
+            }
+            
+            int t= i;
+            if(t>= 10)t%=10;
+            if(answers[i] == third[t]){
+                correct[2]++;
+            }
+            
+            
         }
-        return list.stream().mapToInt(i -> i).toArray();
+        
+        int max = 0;
+        for(int c: correct){
+            max = Math.max(c,max);
+        }
+        List<Integer> result = new ArrayList<>();
+        for(int i =0; i<3; i++){
+            if(correct[i]==max)result.add(i+1);
+        }
+        
+        return result.stream().mapToInt(Integer::intValue).toArray();
     }
 }
