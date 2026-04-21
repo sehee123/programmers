14e5465c143b5f11c5bfa93354d4565ddfaa8d40
hwangsehee <65377385+sehee123@users.noreply.github.com>
2026-04-21 17:03:52 +0900

    [level 2] Title: 카펫, Time: 0.06 ms, Memory: 82.3 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/카펫.java
+++ 프로그래머스/2/42842. 카펫/카펫.java
@@ -1,19 +1,20 @@
-import java.util.*;
 class Solution {
     public int[] solution(int brown, int yellow) {
+        int[] answer = {0,0};
         
         int sum = brown + yellow;
-        
-        for(int i = 1; i<=Math.sqrt(yellow); i++){
-            if(yellow % i== 0){
-                int height = i+2; 
-                int width = (yellow/i)+2;
-               
-                if(height*width == sum){
-                   return new int [] {width, height};
-                } 
+        for(int i=3;i<=sum; i++){
+            if(sum%i==0){
+                int division = sum/i; 
+                if(division<3)continue;
+                if((i-2)*(division-2) == yellow){
+                    answer[0] = Math.max(division,i);
+                    answer[1] = Math.min(division,i);
+                    return answer;
+                }
             }
         }
-		return new int [0];
+        
+        return answer;
     }
 }
