4ede7aabaa24be4808b67ed1cc89180d69a32b7b
hwangsehee <65377385+sehee123@users.noreply.github.com>
2026-05-23 23:27:06 +0900

    [level 3] Title: N으로 표현, Time: 15.05 ms, Memory: 66.9 MB -BaekjoonHub

--- 프로그래머스/3/42895. N으로 표현/N으로 표현.java
+++ 프로그래머스/3/42895. N으로 표현/N으로 표현.java
@@ -2,38 +2,38 @@ import java.util.*;
 
 class Solution {
     public int solution(int N, int number) {
-        int answer = 0;
+        if(N == number)return 1;
         
-        if(N ==number) return 1;
-        
-        Set<Integer>[] dp = new HashSet [9];
-        
-        for(int i =1; i<9; i++){
-            dp[i] = new HashSet<>();
-        }
-        
-        //N, NN, NNN .. 넣어주기
-        int tmp = 0;
-        for(int i = 1; i<9; i++){
-            tmp = tmp*10 +N;
-            dp[i].add(tmp);
-        }
-        
-        //사칙연산 
-        //1+3, 2+2, 3+1 을 생각하기
-        for(int i =2; i<9; i++){
+        //1 : 5 
+        //2 : 55, 25, 1, 0
+        Set<Integer> [] arr = new HashSet [9];
+       
+        for(int i =1; i< 9; i++){
+            Set<Integer> set = new HashSet<>();
+            
+            int repeat = 0; 
+            for(int j =0; j<i; j++){
+                repeat = repeat*10 +N;                
+            }
+            set.add(repeat);
+            
             for(int j= 1; j<i; j++){
-                for(int a :dp[i-j]){
-                    for(int b : dp[j]){
-                        dp[i].add(a+b);
-                        dp[i].add(a-b);
-                        dp[i].add(a*b);
-                        if(b != 0) dp[i].add(a/b);
+                for(int a : arr[j]){
+                    for(int b: arr[i-j]){
+                        set.add(a+b);
+                        set.add(a-b);
+                        set.add(a*b);
+                        if(b != 0)set.add(a/b);
                     }
                 }
             }
-            if(dp[i].contains(number)) return i;
+            
+            if(set.contains(number)) return i;
+            arr[i] = set;
+            
         }
+        
+        
         return -1;
     }
 }
