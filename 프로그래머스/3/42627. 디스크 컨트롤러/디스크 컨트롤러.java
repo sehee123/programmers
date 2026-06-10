40561601db21eb03450d7b91088c015796ff4bd8
hwangsehee <65377385+sehee123@users.noreply.github.com>
2026-06-10 18:09:07 +0900

    [level 3] Title: 디스크 컨트롤러, Time: 2.51 ms, Memory: 79.9 MB -BaekjoonHub

--- 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.java
+++ 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.java
@@ -1,57 +1,44 @@
 import java.util.*;
-class Task { 
-    
-    int idx ; 
-    int start ; 
-    int runTime;
-    
-    public Task(int idx , int start , int runTime){
-        this.idx = idx;
-        this.start = start;
-        this.runTime = runTime;
-    }
-    
-}
 
 class Solution {
     public int solution(int[][] jobs) {
-        int answer = 0;    
-        Task [] taskArray = new Task[jobs.length];
-        for(int i = 0; i<jobs.length; i++){
-            taskArray[i] = new Task(i , jobs[i][0] , jobs[i][1]);
-        }
-        Arrays.sort(taskArray, Comparator.comparingInt(t -> t.start));
+        int answer = 0;
         
-        PriorityQueue <Task> pq = new PriorityQueue<>(
-            Comparator.comparingInt((Task t) -> t.runTime)
-            .thenComparing(t -> t.start)
-            .thenComparing(t -> t.idx)
-        );
+        //요청시각 순으로 정렬
+        Arrays.sort(jobs,(w1,w2)-> Integer.compare(w1[0],w2[0]));
         
-       
-        int time = 0; 
+        //소요시간이 짧은 순 , 작업의 번호가 작은 것
+        PriorityQueue<int[]> pq = new PriorityQueue<>((w1, w2)->{
+            
+            if(w1[0] != w2[0])return Integer.compare(w1[0],w2[0]);
+            
+            return Integer.compare(w1[1],w2[1]);
+        });
+        
+        int count = 0; 
+        int time = 0;
         int sum = 0; 
-        int idx = 0; 
-        int count = 0; //완료된 작업 수 
+        int idx = 0;
         
-       while(count < jobs.length){
-           while(idx < jobs.length && taskArray[idx].start <= time){
-               pq.offer(taskArray[idx]);
-               idx ++;
-           }
-           
-           if(pq.isEmpty()){
-               time = taskArray[idx].start;
-               continue;
-           }
-           
-           Task t = pq.poll();
-           time += t.runTime;
-           sum += time - t.start; 
-           count++;
-           
-       }
-        
+        while(count < jobs.length){           
+            
+            while(idx<jobs.length && jobs[idx][0] <=time){
+                //소요시간,작업번호,요청시간
+                pq.offer(new int []{jobs[idx][1], idx, jobs[idx][0]});
+                idx ++;
+            }
+            
+            if(pq.isEmpty()){
+                time = jobs[idx][0];
+            }else{
+                int [] work = pq.poll();
+                time += work[0];
+                sum += (time - work[2]);
+                count ++;
+            }
+            
+        }
+
         return sum/jobs.length;
     }
 }
