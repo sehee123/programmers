55396ffc2006bb59dee12dcf5fd444a5d3cdf93b
hwangsehee <65377385+sehee123@users.noreply.github.com>
2026-06-10 19:41:07 +0900

    [level 2] Title: 전화번호 목록, Time: 1083.60 ms, Memory: 422 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
@@ -2,19 +2,19 @@ import java.util.*;
 
 class Solution {
     public boolean solution(String[] phone_book) {
-        Set<String> duplCheck = new HashSet<>();
         
-        for(String phone: phone_book){
-            duplCheck.add(phone);
-        }
+        Set<String> set = new HashSet<>();
         
-        for(String phone: phone_book){
-            for(int i =0; i<phone.length(); i++){
-                if(duplCheck.contains(phone.substring(0,i)) == true){
-                    return false;
-                }
+        for(String phone : phone_book){
+            for(int i =1; i<phone.length(); i++){
+                set.add(phone.substring(0,i));
             }
         }
+        
+        for(String phone : phone_book){
+            if(set.contains(phone))return false;
+        }
+                        
         return true;
     }
 }
