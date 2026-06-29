9d6d41dba5aad5bd17c1b9141b9028d41b259d42
hwangsehee <65377385+sehee123@users.noreply.github.com>
2026-06-29 15:21:41 +0900

    [level 2] Title: 이진 변환 반복하기, Time: 2.74 ms, Memory: 79.6 MB -BaekjoonHub

--- 프로그래머스/2/70129. 이진 변환 반복하기/이진 변환 반복하기.java
+++ 프로그래머스/2/70129. 이진 변환 반복하기/이진 변환 반복하기.java
@@ -6,29 +6,19 @@ class Solution {
             
             answer[0]++;
             
-            StringBuilder sb = new StringBuilder();
+            int oneCnt = 0;
+            int len = s.length();
             //0제거
-            for(int i =0; i<s.length(); i++){
+            for(int i =0; i<len; i++){
                 char c = s.charAt(i);
                 if(c == '1'){
-                    sb.append("1");
-                }else{
-                    answer[1]++;
+                    oneCnt ++;
                 }
             }
-         
-            int len = sb.toString().length();
-            int a = 0;
-            int b = 0; 
-            
-            String binary = "";
-            while(len > 0){
-                b = len % 2; 
-                len /= 2;
-                binary = b+ binary;
-            }
-            
-            s = binary;
+            answer [1] += len - oneCnt ; 
+           
+            s = Integer.toBinaryString(oneCnt);
+
         }
         
         return answer;
