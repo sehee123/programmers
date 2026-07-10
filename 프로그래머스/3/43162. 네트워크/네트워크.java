c63203de27d449e39730027e5ceb9883b4f931b7
hwangsehee <65377385+sehee123@users.noreply.github.com>
2026-07-10 23:21:40 +0900

    [level 3] Title: 네트워크, Time: 0.12 ms, Memory: 74.4 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.java
+++ 프로그래머스/3/43162. 네트워크/네트워크.java
@@ -1,24 +1,27 @@
 class Solution {
+    
     boolean [] visited ;
+    
     public int solution(int n, int[][] computers) {
         int answer = 0;
         visited = new boolean [n];
-        
-        for(int i = 0; i<computers.length; i++){
+       
+        for(int i =0; i<n; i++){
             if(!visited[i]){
                 answer ++;
-                dfs(computers,i,n);
+                dfs(i, computers, n);
             }
         }
         
         return answer;
     }
-    public void dfs(int [][] computers,int i, int n ){
-        visited[i] = true;
-       
-        for(int j = 0; j<n; j++){
-            if(!visited[j] && computers[i][j]==1){
-                dfs(computers,j,n);
+    
+    public void dfs(int idx, int [][] computers, int n ){
+        int[] arr = computers[idx];
+        for(int i =0; i<n; i++){
+            if(!visited[i] && arr[i]==1){
+                visited[i] = true;
+                dfs(i,computers,n);
             }
         }
     }
