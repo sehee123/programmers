ceda1cd4d1867461562fc38b594bb8ee2431a031
hwangsehee <65377385+sehee123@users.noreply.github.com>
2026-07-11 22:09:06 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 6.03 ms, Memory: 59 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -5,45 +5,37 @@ class Solution {
         
         int n = maps.length;
         int m = maps[0].length;
-        
-        boolean [][] visited = new boolean [n][m];
-        int [][] distance = new int [n][m];
+        boolean [][]visited = new boolean [n][m];
         
         Queue<int []> q = new LinkedList<>();
-        int [] start = {0,0};
-        q.offer(start);
-        distance [0][0] = 1;
+        q.offer(new int []{0,0,1});
         
         while(!q.isEmpty()){
-            
             int [] current = q.poll();
-            int x = current[0]; 
+            int x = current[0];
             int y = current[1];
+            int cnt = current[2];
             
-            //visited[x][y] = true;
-            //System.out.println(x + " : " + y);
-            if(x == n-1 && y == m-1) return distance[x][y];
-            //상하좌우
-            int [] dx = {1,-1,0,0};
-            int [] dy = {0,0,1,-1};
-            
-            for(int i =0; i<4; i++){
-                int nextX = x + dx[i];
-                int nextY = y + dy[i];
-                
-                if(nextX>-1 && nextX<n && nextY>-1 && nextY<m && !visited[nextX][nextY]){
-                    if(maps[nextX][nextY] == 1){
-                        int [] next = {nextX,nextY};
-                        distance [nextX][nextY] = distance[x][y] + 1;
-                        q.offer(next);
-                        visited[nextX][nextY]= true;
-                    }
-                }
-                
+            if(x==n-1 && y ==m-1){
+                return cnt;
             }
             
+            int [] dx = new int []{1,-1,0,0};
+            int [] dy = new int []{0,0,1,-1};
             
-            
+            for(int i =0; i<4; i++){
+                int moveX = x + dx[i];
+                int moveY = y + dy[i];
+                
+                if(moveX < 0 || moveY<0 || moveX >=n || moveY >=m){
+                    continue;
+                }
+                
+                if(maps[moveX][moveY] == 1 && !visited[moveX][moveY]){
+                    visited[moveX][moveY] = true;
+                    q.offer(new int []{moveX,moveY,cnt+1});
+                }
+            }
         }
         
         return -1;
