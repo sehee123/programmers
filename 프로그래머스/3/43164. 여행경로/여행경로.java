6d2464e9e680d5b0675320e35537778fa87173d0
hwangsehee <65377385+sehee123@users.noreply.github.com>
2026-07-15 20:35:57 +0900

    [level 3] Title: 여행경로, Time: 3.46 ms, Memory: 81.6 MB -BaekjoonHub

--- 프로그래머스/3/43164. 여행경로/여행경로.java
+++ 프로그래머스/3/43164. 여행경로/여행경로.java
@@ -1,31 +1,29 @@
 import java.util.*;
 
 class Solution {
-    List<String> answer = new ArrayList<>();
-    //출발지별 목적지 자동정렬해서 넣기 
-    Map<String, PriorityQueue<String>> flights = new HashMap<>();
+    List<String> answer;
+    Map<String,PriorityQueue<String>> map;
     public String[] solution(String[][] tickets) {
+       
+        answer = new ArrayList<>();
+        map = new HashMap<>();
         
         for(String [] ticket : tickets){
-            flights.computeIfAbsent
-                (ticket[0], t-> new PriorityQueue<>()).add(ticket[1]);
-            }
-        String start = "ICN";
-
-        dfs(start);
+            map.computeIfAbsent(ticket[0],t-> new PriorityQueue<>()).offer(ticket[1]);
+        }
+        
+        dfs("ICN");
         
         return answer.toArray(new String[0]);
     }
+    
     public void dfs(String start){
-            
-            PriorityQueue<String> pq= flights.get(start);
-            while(pq != null &&!pq.isEmpty()){
-                 String destination = pq.poll();
-                 dfs(destination);
-            }
-        //while문이 끝나야 티켓을 다 쓴 것.
-        //역순으로 add 해주기 -> visited 필요 없다
-            answer.add(0,start);
-            
+        PriorityQueue<String> q = map.get(start);
+        
+        while(q !=null && !q.isEmpty()){
+            String next = q.poll();
+            dfs(next);
+        }
+        answer.add(0,start);
     }
 }
