7c695ec3425d4d32694d26475cb4ccee6df62787
hwangsehee <65377385+sehee123@users.noreply.github.com>
2026-07-28 00:00:11 +0900

    [level 3] Title: 섬 연결하기, Time: 4.17 ms, Memory: 74.9 MB -BaekjoonHub

--- 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.java
+++ 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.java
@@ -1,47 +1,42 @@
 import java.util.*;
+
 class Solution {
     public int solution(int n, int[][] costs) {
         int answer = 0;
         
-        // cost 순으로 정렬 
-        Arrays.sort(costs, (a1, a2) -> Integer.compare(a1[2],a2[2]));
-        
-        //초기 무리설정  
-        int [] group = new int [n];
+        List<List<Node>> list = new ArrayList<>();
         for(int i =0; i<n; i++){
-            group [i] = i; 
+            list.add(new ArrayList<>());
         }
         
-        int count = 0; 
-        //적은 비용부터 다리 짓기 
-        for(int [] arr : costs){
-            int b1 = arr[0];
-            int b2 = arr[1];
-            int cost = arr[2];
+        for(int [] cost : costs){
+            list.get(cost[0]).add(new Node(cost[1],cost[2]));
+            list.get(cost[1]).add(new Node(cost[0],cost[2]));
+        }
+        
+        
+        boolean [] visited = new boolean [n];
+        
+        PriorityQueue<Node> pq = new PriorityQueue<>(Comparator.comparing(Node::cost));
+        //0에서 시작 
+        pq.offer(new Node(0, 0));
+        while(!pq.isEmpty()){
+            Node next = pq.poll();
             
-            //root 찾기 
-            int leader1 = findLeader(b1,group);
-            int leader2 = findLeader(b2,group);
-            
-            //같은 무리가 아니면 합치기
-            if(leader1 != leader2){
-                group[leader2] = leader1;
-                answer += cost;
-                count ++; //다리 count
+            if(visited[next.idx]){
+                continue;
             }
-            if(count == n-1) return answer;
             
+            visited[next.idx] = true;
+            answer += next.cost;
+            
+            for(Node node : list.get(next.idx)){
+                pq.offer(node);
+            }
         }
         
         return answer;
     }
     
-    public int findLeader (int node, int [] group){
-       if(group[node] == node) return node;
-        
-        //찾은김에 root 넣어주기 
-        group[node] = findLeader(group[node],group);
-        
-        return group[node];
-    }
+    record Node(int idx, int cost){}
 }
