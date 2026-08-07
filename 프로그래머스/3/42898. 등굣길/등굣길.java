06f4535926edcf688fc2719adc44483504639666
hwangsehee <65377385+sehee123@users.noreply.github.com>
2026-08-07 16:30:36 +0900

    [level 3] Title: 등굣길, Time: 1.03 ms, Memory: 57.6 MB -BaekjoonHub

--- 프로그래머스/3/42898. 등굣길/등굣길.java
+++ 프로그래머스/3/42898. 등굣길/등굣길.java
@@ -1,31 +1,38 @@
+import java.util.*;
+
 class Solution {
     public int solution(int m, int n, int[][] puddles) {
         int answer = 0;
         
-        int [][] cost = new int [m][n];
-        boolean [][] isPuddles = new boolean [m][n];
-        cost[0][0] = 1;
+        int [][] map = new int [n+1][m+1];
         
-        for(int [] puddle : puddles){
-            isPuddles[puddle[0]-1][puddle[1]-1] = true;
-            //웅덩이 표시
+        for(int [] p : puddles){
+            map[p[1]][p[0]] = -1;
         }
         
-        for(int i =0; i<m; i++){
-            for(int j =0; j<n; j++){
-                if(i == 0 && j==0) continue;
-                if(isPuddles[i][j]) continue;
+        //map [i][j] = map[i-1][j] + map[i][j-1];
+        int [] dx = new int []{-1,0};
+        int [] dy = new int []{0,-1};
+        
+        map[1][1] = 1;
+        
+        for(int i =1; i<n+1; i++){
+            for(int j =1; j<m+1; j++){
                 
-                if(i == 0){
-                    cost[i][j] = cost[i][j-1];
-                }else if(j == 0){
-                    cost[i][j] = cost[i-1][j];
-                }else {
-                     cost[i][j] = (cost[i-1][j] + cost[i][j-1])%1000000007;
+                if(map[i][j] == -1) continue;
+                
+                int count = 0; 
+                for(int k = 0; k<2; k++){
+                    int nx = i+dx[k];
+                    int ny = j+dy[k];
+                    if(nx>0 && nx<n+1 && ny>0 && ny<m+1 && map[nx][ny] != -1){
+                        count += map[nx][ny];
+                    }   
                 }
+                map[i][j] += count%1000000007;
             }
         }
         
-        return cost[m-1][n-1];
+        return map[n][m];
     }
 }
